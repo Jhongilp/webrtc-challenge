c558669794bcef6d710964c1d844af7ea735cb08
Jonathan Gil <jhongilp@gmail.com>
2022-02-15 21:04:17 -0500

    create offer

--- client/src/components/room/Room.tsx
+++ client/src/components/room/Room.tsx
@@ -26,11 +26,71 @@ const Room = () => {
   const localVideoRef = useRef<any>(null);
   const remoteVideoRef = useRef<any>(null);
   const localStreamRef = useRef<any>(null);
+  const remoteStreamRef = useRef<any>(null);
+
   const isInitiator = useRef(false);
   const isChannelReady = useRef(false);
   const isStarted = useRef(false);
+  const pc = useRef<any>(null);
 
-  const maybeStart = () => {
+  const handleRemoteStreamAdded = useCallback((event: any) => {
+    console.log("Remote stream added.");
+    remoteStreamRef.current = event.stream;
+    remoteVideoRef.current.srcObject = remoteStreamRef.current;
+  }, []);
+
+  const handleRemoteStreamRemoved = useCallback((event: any) => {
+    console.log("Remote stream removed. Event: ", event);
+  }, []);
+
+  const handleIceCandidate = useCallback((event: any) => {
+    console.log("icecandidate event: ", event);
+    if (event.candidate) {
+      sendMessage({
+        type: "candidate",
+        label: event.candidate.sdpMLineIndex,
+        id: event.candidate.sdpMid,
+        candidate: event.candidate.candidate,
+      });
+    } else {
+      console.log("End of candidates.");
+    }
+  }, []);
+
+  const createPeerConnection = useCallback(() => {
+    try {
+      pc.current = new RTCPeerConnection(pcConfig);
+      pc.current.onicecandidate = handleIceCandidate;
+      pc.current.onaddstream = handleRemoteStreamAdded;
+      pc.current.onremovestream = handleRemoteStreamRemoved;
+      console.log("Created RTCPeerConnnection");
+    } catch (e: any) {
+      console.log(`Failed to create PeerConnection, exception: ${e.message}`);
+      alert("Cannot create RTCPeerConnection object.");
+    }
+  }, [handleIceCandidate, handleRemoteStreamAdded, handleRemoteStreamRemoved]);
+
+  const handleCreateOfferError = useCallback((event: any) => {
+    console.log("createOffer() error: ", event);
+  }, []);
+
+  const setLocalAndSendMessage = useCallback((sessionDescription: any) => {
+    // Set Opus as the preferred codec in SDP if Opus is present.
+    //  sessionDescription.sdp = preferOpus(sessionDescription.sdp);
+    pc.current.setLocalDescription(sessionDescription);
+    console.log("setLocalAndSendMessage sending message", sessionDescription);
+    sendMessage(sessionDescription);
+  }, []);
+
+  const doCall = useCallback(() => {
+    console.log("Sending offer to peer");
+    pc.current
+      .createOffer([sdpConstraints])
+      .then((offer: any) => setLocalAndSendMessage(offer))
+      .catch(handleCreateOfferError);
+  }, [setLocalAndSendMessage, handleCreateOfferError]);
+
+  const maybeStart = useCallback(() => {
     console.log(
       ">>>>>>> maybeStart() ",
       isStarted.current,
@@ -43,32 +103,34 @@ const Room = () => {
       isChannelReady.current
     ) {
       console.log(">>>>>> creating peer connection");
-      // createPeerConnection();
-      // pc.addStream(localStream);
-      // isStarted = true;
-      // console.log('isInitiator', isInitiator);
-      // if (isInitiator) {
-      //   doCall();
-      // }
+      createPeerConnection();
+      pc.current.addStream(localStreamRef.current); // TODO should I move this inside createPeerConnection()?
+      isStarted.current = true;
+      console.log("isInitiator", isInitiator.current);
+      if (isInitiator.current) {
+        doCall();
+      }
     }
-  };
+  }, [createPeerConnection, doCall]);
 
-  const gotStream = useCallback((stream: any) => {
-    console.log("Adding local stream.");
-    localStreamRef.current = stream;
-    localVideoRef.current.srcObject = stream;
-    sendMessage("got user media");
-    if (isInitiator.current) {
-      maybeStart();
-    }
-  }, []);
+  const gotStream = useCallback(
+    (stream: any) => {
+      console.log("Adding local stream.");
+      localStreamRef.current = stream;
+      localVideoRef.current.srcObject = stream;
+      sendMessage("got user media");
+      if (isInitiator.current) {
+        maybeStart();
+      }
+    },
+    [maybeStart]
+  );
 
   useEffect(() => {
     socket.emit("create or join", "test");
 
     socket.on("created", (roomObject, socketId) => {
       console.log(`Created room ${roomObject} - socketId: ${socketId}`);
-      // isInitiator = true;
       isInitiator.current = true;
     });
 
@@ -91,11 +153,11 @@ const Room = () => {
       console.log(...array);
     });
 
-    socket.on('message', (message) => {
-      console.log('Client received message:', message);
-      if (message === 'got user media') {
+    socket.on("message", (message) => {
+      console.log("Client received message:", message);
+      if (message === "got user media") {
         maybeStart();
-      } 
+      }
       // else if (message.type === 'offer') {
       //   if (!isInitiator && !isStarted) {
       //     maybeStart();
@@ -115,14 +177,13 @@ const Room = () => {
       // }
     });
 
-
     navigator.mediaDevices
       .getUserMedia(constraints)
       .then(gotStream)
       .catch((e) => {
         alert(`getUserMedia() error: ${e.name}`);
       });
-  }, [gotStream]);
+  }, [gotStream, maybeStart]);
 
   const sendMessage = (message: any) => {
     console.log("Client sending message: ", message);
